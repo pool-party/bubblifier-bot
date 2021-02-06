3ed91ecf4bcae9fff8a145e9b92f833fe0937ae6
Simon Naumov <daretoodefy@gmail.com>
2021-02-06 15:47:40 +0000

    :sparkles: creating sticker pack (#2)
    * :sparkles: creating sticker pack
    
    * :pencil: updating readme
    
    * :card_file_box: adding database orm
    
    * :see_no_evil: improving gitignore file
    
    * :truck: moving logo file into assets directory
    
    * :pencil: fixing readme links
    
    * :rotating_light: fix lint
    
    * :art: using prettier, as we should
    
    Co-authored-by: ShuffleZZZ <mihpihnaty@yandex.ru>

--- README.md
+++ README.md
@@ -2,23 +2,55 @@
 
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square&logo=prettier)](https://github.com/prettier/prettier)
 [![CodeFactor Grade](https://img.shields.io/codefactor/grade/github/pool-party/bubblifier-bot?logo=codefactor)](https://www.codefactor.io/repository/github/pool-party/bubblifier-bot)
-[![TypeScript Lang](https://img.shields.io/github/languages/top/pool-party/bubblifier-bot?logo=TypeScript)](https://typescriptlang.org/)
+[![TypeScript Lang](https://img.shields.io/github/languages/top/pool-party/bubblifier-bot?logo=TypeScript)](http://typescriptlang.org/)
 [![Bubblifier Bot](https://img.shields.io/badge/telegram-Bubblifier_Bot-blue?logo=Telegram)](https://t.me/BubblifierBot/)
 
 ## Usage guide
 
-### Building up a database
+Add [`@BubblifierBot`](https://t.me/BubblifierBot) to the chat to be able to create custom bubble sticker packs using
+following commands:
 
-+ Install [Prisma](https://prisma.io/)
-+ Run `prisma generate` to compile a custom `@prisma/client` to be used in code
-+ Provide a database url in `DATABASE_URL` environment variable
-+ Run `prisma migrate dev --preview-feature` to migrate your database
++ `/bubble` - on first invocation it both creates a new sticker pack for the chat
+  and adds referenced message bubble sticker, on the following invocations it only adds new bubble stickers to the
+  existing pack
 
 ## Tools and Libraries
 
++ [TypeScript](http://typescriptlang.org/) language
+
++ [npm](https://npmjs.com/) - JavaScript programming language package manager
+
++ [Prettier](https://prettier.io/) - An opinionated code formatter
+
++ [ESLint](https://eslint.org/) - Static code analyzer
+
++ [Emoji commit messages guide](https://gitmoji.dev/)
+
 + [Prisma](https://prisma.io/) - Next-generaion ORM for Node.js and TypeScript
+
 + [Telegraf](https://telegraf.js.org/) - Modern Telegram Bot Framework for Node.js
+
++ [sharp](https://github.com/lovell/sharp/) - High performance Node.js image processing,
+  the fastest module to resize JPEG, PNG, WebP, AVIF and TIFF images
+
++ [got](https://github.com/sindresorhus/got/) - Human-friendly and powerful HTTP request library for Node.js
 
 ## Launching
 
+### Building up a database
+
++ Install [Prisma](https://prisma.io/)
+
++ Run `prisma generate` to compile a custom `@prisma/client` to be used in code
+
++ Provide a database url in `DATABASE_URL` environment variable
+
++ Run `prisma migrate dev --preview-feature` to migrate your database
+
+### Environment variables
+
++ Provide `DATABASE_URL` with database url you want to use
+
 + Provide `BOT_TOKEN` - the one you get from [BotFather](https://t.me/BotFather)
+
++ Provide `BOT_NAME` - the registered bot name
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,117 @@
 import { Telegraf } from "telegraf";
 import { BotCommand } from "telegraf/src/telegram-types";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, StickerPack } from "@prisma/client";
+import sharp from "sharp";
+import got from "got";
 
 const token = process.env.BOT_TOKEN;
 if (!token) {
   throw new Error("BOT_TOKEN must be provided");
 }
 
+const botName = process.env.BOT_NAME;
+if (!botName) {
+  throw new Error("BOT_NAME must be provided");
+}
+
 const bot = new Telegraf(token);
 
 const prisma = new PrismaClient({
-  log: [{ emit: 'event', level: 'query' }]
+  log: [{ emit: "event", level: "query" }],
 });
 
-bot.use(async (ctx, next) => {
-  const updateString = `Processing ${ctx.updateType} (${ctx.update.update_id}) in ${ctx.chat?.id}`;
-  console.time(updateString);
-  await next();
-  console.timeEnd(updateString);
+bot.command("bubble", async (ctx) => {
+  const chatId = ctx.chat?.id;
+
+  if (!chatId) {
+    return;
+  }
+
+  let stickerPack = await prisma.stickerPack.findUnique({ where: { chatId: chatId } });
+
+  if (!stickerPack) {
+    const sender = ctx.from?.id;
+
+    if (!sender) {
+      console.log("No sender id is provided");
+      return;
+    }
+
+    const chat = ctx.chat;
+    if (!chat) {
+      console.log("No chat is provided somehow");
+      return;
+    }
+
+    const chatPhotoId = await ctx.getChat().then((chat) => chat.photo?.small_file_id);
+    if (!chatPhotoId) {
+      console.log("No chat photo is provided");
+      return;
+    }
+
+    const chatPhotoDownloadLink = await ctx.telegram.getFileLink(chatPhotoId);
+    if (!chatPhotoDownloadLink) {
+      console.log("No chat photo download path is provided");
+      return;
+    }
+
+    let title: string;
+
+    switch (chat.type) {
+      case "group":
+      case "channel":
+      case "supergroup":
+        title = chat.title;
+        break;
+      case "private": {
+        const lastName = chat.last_name;
+        title = `${chat.first_name}${lastName ? ` ${lastName}` : ""}`;
+      }
+    }
+    title = `${title} bubbles`;
+
+    const stickerPackName = `${chatId.toString().replace("-", "minus")}_by_${botName}`;
+    console.log(
+      `Creating sticker pack "${title}" (https://t.me/addstickers/${stickerPackName}) with ${ctx.from?.username} owner`
+    );
+
+    await ctx
+      .createNewStickerSet(stickerPackName, title, {
+        png_sticker: { source: "assets/logo-512.png" },
+        emojis: "💭",
+      })
+      .catch((error) => {
+        console.error(error);
+        ctx.reply("Failed to create a new sticker pack");
+      });
+
+    const newStickerPack: StickerPack = {
+      chatId: BigInt(chatId),
+      userId: BigInt(sender),
+      name: stickerPackName,
+    };
+
+    await prisma.stickerPack.create({ data: newStickerPack });
+    stickerPack = newStickerPack;
+
+    const photoBuffer = await got
+      .stream(chatPhotoDownloadLink)
+      .pipe(sharp({ failOnError: false }))
+      .resize(100, 100)
+      .toBuffer();
+
+    const settingThumbResult = await ctx.setStickerSetThumb(stickerPackName, sender, { source: photoBuffer });
+    console.log(`Setting sticker pack thumb: ${settingThumbResult}`);
+  }
+
+  ctx.replyWithSticker((await ctx.getStickerSet(stickerPack.name)).stickers[0].file_id);
 });
 
-bot.command("bubble", async (/*ctx*/) => {
-  // TODO
-  // const chat = await ctx.getChat();
-  // await ctx.createNewStickerSet(chat.id.toString(), chat.id.toString());
-});
-
-// TODO
 bot.settings(async (ctx) => {
   const commands: readonly BotCommand[] = [
     {
       command: "/bubble",
-      description: "some description",
+      description: "create a bubble sticker",
     },
     {
       command: "/help",
@@ -52,6 +132,13 @@ bot.help(async (ctx) => {
   const commands = await ctx.getMyCommands();
   const info = commands.map((command) => `/${command.command} - ${command.description}`).join("\n");
   return ctx.reply(`Available commands:\n${info}`);
+});
+
+bot.use(async (ctx, next) => {
+  const updateString = `Processing ${ctx.updateType} (${ctx.update.update_id}) in ${ctx.chat?.id}`;
+  console.time(updateString);
+  await next();
+  console.timeEnd(updateString);
 });
 
 bot
