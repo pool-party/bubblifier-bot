33f98af5f0dc612e413f4a3b4c5d87c2ca9be525
Simon Naumov <daretoodefy@gmail.com>
2021-06-08 15:50:20 +0300

    :white_check_mark: adding, checking, improving selenium support (#7)
    * :sparkles: introducing selenoid
    
    * :art: improving structure, updating libraries
    
    * :white_check_mark: adding, checking, improving selenium support

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,35 +2,40 @@
 extern crate diesel;
 
 use diesel::{pg::PgConnection, prelude::*};
+use std::result::Result;
 use std::sync::{Arc, Mutex};
 use teloxide::{dispatching::*, prelude::*, utils::command::BotCommand};
+use tokio_stream::wrappers::UnboundedReceiverStream;
 
 pub mod bubble;
 pub mod models;
 pub mod schema;
 pub mod settings;
+pub mod utils;
 
 use self::settings::Settings;
+use self::utils::Clown;
 
-fn establish_connection() -> Arc<Mutex<PgConnection>> {
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+fn establish_connection(url: String) -> Arc<Mutex<PgConnection>> {
     Arc::new(Mutex::new(
-        PgConnection::establish(&database_url)
-            .expect(&format!("Error connecting to {}", database_url)),
+        PgConnection::establish(&url).expect(&format!("Error connecting to {}", url)),
     ))
 }
 
 #[derive(BotCommand)]
-#[command(rename = "lowercase", description = "Available commands")]
+#[command(rename = "lowercase", description = "Available commands:\n")]
 enum Command {
+    #[command(description = "start bubblifying")]
+    Start,
     #[command(description = "display this text")]
     Help,
     #[command(description = "create a bubble sticker")]
     Bubble,
 }
 
-async fn answer(
-    context: UpdateWithCx<Message>,
+async fn answer<R: Requester>(
+    bot: Bot,
+    context: UpdateWithCx<R, Message>,
     command: Command,
     settings: Arc<Settings>,
     connection: Arc<Mutex<PgConnection>>,
@@ -38,13 +43,16 @@ async fn answer(
     log::info!("Processing {:?} from {}", context.update.text(), context.chat_id());
 
     match command {
+        Command::Start => {
+            context.answer("bot started").send().await.clown()?;
+        }
         Command::Help => {
-            context.answer(Command::descriptions()).send().await?;
+            context.answer(Command::descriptions()).send().await.clown()?;
         }
         Command::Bubble => {
-            if let Err(err) = bubble::bubble(&context, settings, connection).await {
+            if let Err(err) = bubble::bubble(bot.clone(), &context, settings, connection).await {
                 log::error!("{}", err);
-                context.answer(format!("Error: {}", err)).send().await?;
+                context.answer(format!("Error: {}", err)).send().await.clown()?;
             }
         }
     };
@@ -56,39 +64,38 @@ async fn run() {
     dotenv::dotenv().ok();
     let settings = match Settings::new() {
         Ok(s) => Arc::new(s),
-        Err(r) => panic!(r),
+        Err(r) => panic!("{}", r),
     };
 
     teloxide::enable_logging_with_filter!(log::LevelFilter::Debug);
     log::info!("Starting bot...");
 
-    let bot = teloxide::BotBuilder::new().token(settings.teloxide.token.clone()).build();
+    let bot = teloxide::Bot::new(settings.teloxide.token.clone());
 
     let handler = Arc::new(answer);
-    let connection = establish_connection();
+    let connection = establish_connection(settings.database.url.clone());
     let cloned_bot = bot.clone();
 
-    Dispatcher::new(bot)
-        .messages_handler(move |rx: DispatcherHandlerRx<Message>| {
-            rx.commands(settings.teloxide.name.clone()).for_each_concurrent(
-                None,
-                move |(context, command)| {
+    Dispatcher::new(bot.clone())
+        .messages_handler(move |rx: DispatcherHandlerRx<_, Message>| {
+            UnboundedReceiverStream::new(rx)
+                .commands(settings.teloxide.name.clone())
+                .for_each_concurrent(None, move |(message, command)| {
                     let handler = handler.clone();
-
                     let connection_clone = connection.clone();
                     let settings_clone = settings.clone();
+                    let bot_clone = cloned_bot.clone();
 
                     async move {
-                        handler(context, command, settings_clone, connection_clone)
+                        handler(bot_clone, message, command, settings_clone, connection_clone)
                             .await
                             .log_on_error()
                             .await;
                     }
-                },
-            )
+                })
         })
         .dispatch_with_listener(
-            update_listeners::polling_default(cloned_bot),
+            update_listeners::polling_default(bot),
             teloxide::error_handlers::LoggingErrorHandler::with_custom_text(
                 "An error from the update listener",
             ),
